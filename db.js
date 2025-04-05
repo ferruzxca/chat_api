5a8bab001df705df8e4fff52641d51826a8e78ac
ferruzxca <43899379+ferruzxca@users.noreply.github.com>
2025-04-04 19:22:32 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,20 +1,21 @@
+require('dotenv').config(); // ¡Necesario para Render!
+
 const mysql = require('mysql2');
-require('dotenv').config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 8889
+    port: 3306
 });
 
 db.connect((err) => {
     if (err) {
-        console.error('Error de conexión a la BD:', err.message);
+        console.error('Error de conexión a la base de datos:', err.message);
         return;
     }
-    console.log('Conexión a la base de datos MySQL exitosa!');
+    console.log('Conexión a la base de datos MySQL exitosa.');
 });
 
 module.exports = db;
