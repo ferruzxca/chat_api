6c1d5fe8cb716b1d9d08ed36ca952860d84ac135
ferruzxca <43899379+ferruzxca@users.noreply.github.com>
2025-04-04 19:22:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -9,18 +10,25 @@ const usuarioRoutes = require('./routes/usuarios');
 const mensajeRoutes = require('./routes/mensajes');
 
 const app = express();
+
+// Render ignora esta variable, pero la necesitamos para local
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors()); // muy importante para que Android pueda acceder
-app.use(express.json());
+app.use(cors()); // Permite acceso desde Android y frontend
+app.use(express.json()); // Permite recibir JSON en requests
 
 // Endpoints base
 app.use('/api/auth', authRoutes);
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/mensajes', mensajeRoutes);
 
-// Inicio del servidor
+// Ruta raíz opcional
+app.get('/', (req, res) => {
+    res.send('API REST Chat activa y funcionando.');
+});
+
+// Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Servidor API escuchando en el puerto ${PORT}`);
 });
