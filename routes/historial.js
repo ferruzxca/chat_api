8ef1984a99bd6ca82e23fae9545281a71b1138e9
ferruzxca <ferruzxca@gmail.com>
2025-04-05 09:33:10 -0600

    updated

--- routes/historial.js
+++ routes/historial.js
@@ -1,28 +1,24 @@
 // routes/historial.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db');
+const db = require("../db");
 
 // Ruta: /historial
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const [mensajes] = await db.promise().query(`
-      SELECT 
-        LEAST(emisor_id, receptor_id) AS usuario1,
-        GREATEST(emisor_id, receptor_id) AS usuario2,
-        MAX(fecha_envio) AS ultima_fecha,
-        SUBSTRING_INDEX(GROUP_CONCAT(mensaje ORDER BY fecha_envio DESC), ',', 1) AS ultimo_mensaje
-      FROM 
-        mensajes
-      GROUP BY 
-        LEAST(emisor_id, receptor_id), GREATEST(emisor_id, receptor_id)
-      ORDER BY 
-        ultima_fecha DESC
-    `);
-
+        SELECT 
+      DATE_FORMAT(m.fecha_envio, '[%d/%m/%y, %l:%i:%s %p]') AS fecha_formateada,
+      u.nombre AS emisor_nombre,
+      m.mensaje
+  FROM mensajes m
+  JOIN usuarios u ON m.emisor_id = u.id
+  WHERE (m.emisor_id = ? AND m.receptor_id = ?) 
+     OR (m.emisor_id = ? AND m.receptor_id = ?)
+  ORDER BY m.fecha_envio ASC`);
 
     res.json(mensajes);
-   } catch (err) {
+  } catch (err) {
     console.error("ERROR EN /historial:", err); // Mensaje más claro
     res.status(500).json({ error: err.message });
   }
