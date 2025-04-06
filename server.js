e2f3865f4194cce6b72e3dde291e30a0c4595321
ferruzxca <ferruzxca@gmail.com>
2025-04-06 11:14:28 -0600

    Update

--- server.js
+++ server.js
@@ -11,15 +11,17 @@ const usuarioRoutes = require('./routes/usuarios');
 const mensajeRoutes = require('./routes/mensajes');
 const historialRoutes = require('./routes/historial');
 
-
-
-
 // Render ignora esta variable, pero la necesitamos para local
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors()); // Permite acceso desde Android y frontend
 app.use(express.json()); // Permite recibir JSON en requests
+/* The line `app.use(express.urlencoded({ extended: true }));` is setting up a middleware in the
+Express application to parse incoming requests with URL-encoded payloads. When a form is submitted
+with POST method and enctype set to 'application/x-www-form-urlencoded', the data is sent in the
+body of the request in a URL-encoded format. */
+app.use(express.urlencoded({ extended: true }));
 
 // Endpoints base
 app.use('/api/auth', authRoutes);
